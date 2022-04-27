7328c26c7de67fb37f9e4078fd8451d03ffdffd8
jvaquero <jvaquero@aidacanarias.com>
2022-04-27 07:56:50 +0100

    test move_forward_one_unit_when_facing_north

--- MarsRoverKata.Tests/MarsRoverShould.cs
+++ MarsRoverKata.Tests/MarsRoverShould.cs
@@ -34,13 +34,15 @@ namespace MarsRoverKata.Tests
         [Test]
         public void move_forward_one_unit_when_facing_north()
         {
-            var rover = new MarsRover(2, 2, "N");
+            var randomizer = new Random(DateTime.Now.Millisecond);
+            var randomY = randomizer.Next(2, 5);
+            var rover = new MarsRover(2, randomY, "N");
 
             var result = rover.ExecuteCommand("f.1");
             
-            result.Should().Be("2,1");
+            result.Should().Be($"2,{randomY-1}");
         }
-
+        
         [Test]
         public void move_backward_one_unit_when_facing_north()
         {
